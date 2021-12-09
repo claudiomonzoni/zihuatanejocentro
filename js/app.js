b0acf1c331cc0605ba3f5c6836c3b8fd9ad6d7e0
claudiomonzoni <claudiomonzoni@hotmail.com>
2021-12-09 10:33:56 -0600

    avance, resta funciones y seo

--- js/app.js
+++ js/app.js
@@ -1,9 +1,33 @@
 
 import gsap from "gsap";
+
 const cargando = document.getElementById("cargando");
 window.onload = function () {
   cargando.classList.add("eliminado");
 };
+
+
+//only phones
+var x = window.matchMedia("(max-width: 479px)");
+esCell(x); // Call listener function at run time
+x.addListener(esCell); // Attach listener function on state changes
+
+// nav
+
+function esCell(x) {
+console.log("Soy celular")
+}
+
+// agregar el shift al scroll
+
+// const habitacion= document.querySelector('#habitaciones')
+// habitacion.addEventListener('mouseover', (e) => {
+//   console.log("que")
+//   // new KeyboardEvent('keydown', {'key':'Shift'} )
+// } )
+// let scrollVertical = new Event('click')
+// habitacion.dispatchEvent(scrollVertical)
+
 
 // importo gsap
 // // llamo gspa
@@ -26,66 +50,6 @@ gsap.from(".punto", {
   duration: 0.6, //1 segundo
 });
 
-
-
-//only phones
-var x = window.matchMedia("(max-width: 479px)");
-esCell(x); // Call listener function at run time
-x.addListener(esCell); // Attach listener function on state changes
-
-// nav
-
-function esCell(x) {
-  if (x.matches) {
-    // If media query matches
-    const menuIco = document.querySelector(".menu-toggle");
-    const mainNav = document.querySelector("#nav");
-    // const anchor = document.querySelector(".anchor");
-    const abierto = false;
-
-    function abrirMenuFull() {
-      this.abierto = !this.abierto;
-      mainNav.classList.remove("menuFullAbierto");
-      mainNav.classList.remove("desanimaMenu");
-      menuIco.classList.toggle("is-active");
-
-      this.abierto
-        ? mainNav.classList.toggle("menuFullAbierto")
-        : mainNav.classList.toggle("desanimaMenu");
-    }
-
-    menuIco.addEventListener("click", abrirMenuFull);
-    //anchor.addEventListener("click", abrirMenuFull);
-  } else {
-    return;
-  }
-}
-
-
-
-//menu efecto al hacer scroll
-const body = document.getElementById("nav");
-let lastScroll = 0;
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= 0) {
-    body.classList.remove("scroll-up");
-    return;
-  }
-
-  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-    body.classList.remove("scroll-up");
-    body.classList.add("scroll-down");
-  } else if (
-    currentScroll < lastScroll &&
-    body.classList.contains("scroll-down")
-  ) {
-    body.classList.remove("scroll-down");
-    body.classList.add("scroll-up");
-  }
-  lastScroll = currentScroll;
-});
 
 const WOW = require("wowjs");
 
